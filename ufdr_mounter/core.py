0f40226d86771afb918f4afba340ba82491cdcef
SribatschaMaharana <sribatscha.maharana@gmail.com>
2025-05-05 13:19:24 -0400

    update windows functionality

--- ufdr_mounter/core.py
+++ ufdr_mounter/core.py
@@ -16,11 +16,16 @@ def get_gid():
     return os.getgid()
 
 def validate_mount_point(mount_point: str):
-    if not os.path.exists(mount_point):
-        os.makedirs(mount_point)
-    if not os.path.isdir(mount_point):
-        raise ValueError(f"Mount point {mount_point} is not a directory.")
-
+    system = platform.system()
+    if system == "Windows":
+        if not (len(mount_point) == 2 and mount_point[1] == ":" and mount_point[0].isalpha()):
+            raise ValueError("On Windows, mount point must be a drive letter like 'M:'.")
+    else:
+        if not os.path.exists(mount_point):
+            os.makedirs(mount_point)
+        if not os.path.isdir(mount_point):
+            raise ValueError(f"Mount point {mount_point} is not a directory.")
+        
 def unmount(mount_point: str):
     system = platform.system()
     log.info(f"Unmounting {mount_point} on {system}")
